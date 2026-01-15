c80e4f5cd34d9cb5a79cd610697aa80e74a1d83a
ProjetCEF <megregent@hotmail.fr>
2026-01-15 08:28:19 +0100

    ebauche HP

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,17 @@
 const Card = (props) => {
     return (
         <div className="col-sm-4">
-            <div className="card text-center">
+            <div className="card">
                 <img src={props.imageLink} class="card-img-top" alt={props.nomArtisan} />
                 <div class="card-body">
                     <h5 className="card-title">{props.nomArtisan}</h5>
-                    <p><i class="bi bi-heart"></i></p>
-                    <p className="card-text"> {props.specialite}</p>
+                    <p className="card-text">
+                    <i class="bi bi-heart"></i> {props.specialite}
+                    </p>
                     <p className="card-text">
                     <i class="bi bi-geo-alt"></i> {props.localisation}
                     </p>
-                    <a href="#!" class="btn btn-primary">Découvrir</a>
+                    <a href="#!" class="btn">Découvrir</a>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import Card from "../components/card"
+import Card from "../components/card";
+import artisan from "../artisan.jpg";
+import ImageHeader from "../assets/undraw_location-search_nesh.svg";
+
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <Card nomArtisan="Nom artisan" specialite="Webdesign" localisation="Lyon" />
+            <div id="HomeHeader" className="row">
+                <div className="col-md-4">
+                    <img src={ImageHeader} alt="Logo" width="300"className="d-inline-block align-text-top"/>
+                </div>    
+                <div className="col-md-8">
+                    <h1>Comment trouver mon artisan ?</h1>
+                    <ol className="h3">
+                        <li>Choisir la catégorie d'artisanat dans le menu</li>
+                        <li>Choisir un artisan</li>
+                        <li>Le contacter via le formulaire de contact</li>
+                        <li>Une réponse sera apportée sous 48h</li>
+                    </ol>
+                </div>     
+            </div>
+            <div>
+                <h2>Les artisans du mois</h2>
+                <div className="row">
+                <Card className="col-md-4" imageLink={artisan} nomArtisan="Nom artisan" specialite="Boulanger" localisation="Lyon" />
+                <Card className="col-md-4" imageLink={artisan} nomArtisan="Nom artisan" specialite="Boulanger" localisation="Lyon" />
+                <Card className="col-md-4" imageLink={artisan} nomArtisan="Nom artisan" specialite="Boulanger" localisation="Lyon" />
+                </div>
+            </div>
+            
         </div>
     )
 }
